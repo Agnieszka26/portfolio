5e46288f8f36f5e366064e03142857da205c4740
agnieszkaM <agnieszka.medrek@reikiholding.com>
2024-07-19 14:59:40 +0200

    fix: An API key is required to connect to Airtable

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -1,5 +1,5 @@
 const Airtable = require("airtable");
 
 export const base = new Airtable({
-  apiKey: process.env.NEXT_PUBLIC_PERSONAL_ACCESS_TOKEN_SECRET ?? "",
+  apiKey: process.env.NEXT_PUBLIC_PERSONAL_ACCESS_TOKEN_SECRET ?? " ",
 }).base(process.env.NEXT_PUBLIC_API_TOKEN_BASE);
