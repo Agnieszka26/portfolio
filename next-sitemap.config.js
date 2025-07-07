a210ac7a539e396ce262ed3f8538cb8d290234fd
Agnieszka26 <medrek.agn@yahoo.com>
2025-07-07 14:24:06 +0200

    setup meta tags and files for robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://portfolio-agnieszka26.vercel.app/',
+  generateRobotsTxt: true, 
+}
--- src/app/[locale]/head.tsx
+++ src/app/[locale]/head.tsx
@@ -1,9 +1,44 @@
 export default function Head() {
   return (
     <>
-      <title>AGNA</title>
+      <title>AGNA - Front-end Developer | Portfolio</title>
+      <meta
+        name="description"
+        content="Portfolio Agnieszka Mędrek - front-end developer, Next.js, React, UI/UX. Zobacz moje projekty, umiejętności i kontakt."
+      />
+
+      <meta charSet="UTF-8" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta property="og:title" content="Portfolio" key="title" />
+      <link rel="canonical" href="https://portfolio-agnieszka26.vercel.app/" />
+
+      <meta
+        property="og:title"
+        content="Agnieszka Mędrek - Front-end Developer"
+      />
+      <meta
+        property="og:description"
+        content="Przegląd projektów i umiejętności Agnieszki - specjalistki React i Next.js"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://portfolio-agnieszka26.vercel.app/"
+      />
+      <meta property="og:image" content="/og_image.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:title"
+        content="Agnieszka Mędrek- Front-end Developer"
+      />
+      <meta
+        name="twitter:description"
+        content="Portfolio: projekty React, Next.js, UI/UX"
+      />
+      <meta name="twitter:image" content="/twitter_image.png" />
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -26,6 +61,7 @@ export default function Head() {
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="robots" content="index,follow" />
     </>
   );
 }
