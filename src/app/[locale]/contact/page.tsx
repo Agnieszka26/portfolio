693b48b6b6a92c2b27a12cc8955cd681f6a84a05
Agnieszka26 <medrek.agn@yahoo.com>
2026-07-09 16:40:07 +0200

    Contact image missing explicit dimensions

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -31,7 +31,8 @@ const Contact = () => {
             src={image}
             alt="contact"
             className={styles.image}
-
+            width={500}
+            height={500}
           />
         </div>
       </Animation>
