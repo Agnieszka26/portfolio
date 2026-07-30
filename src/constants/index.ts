7319c0e9a280d78983c015284ab6225e7e860c3f
Agnieszka26 <medrek.agn@yahoo.com>
2026-07-30 14:17:39 +0200

    Add blog link to navigation in English and Polish
    - Updated the navigation JSON files to include a new "blog" entry for both English and Polish languages.
    - Modified the constants to add the blog route in the navbar elements for improved navigation.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,7 +10,10 @@ export const navbarElements = [
     link: RoutesPath.PROJECTS,
     text: "projects",
   },
-
+  {
+    link: RoutesPath.BLOG,
+    text: "blog",
+  },
   {
     link: RoutesPath.GET_IN_TOUCH,
     text: "get_in_touch",
